81cf85f93b2479b13d03dc07e3a576ec7ebb4963
lenarayneallen <124638335+lenarayneallen@users.noreply.github.com>
2023-04-09 21:59:35 -0400

    Create HAMM-code.py

---
+++ Bioinformatics-stronghold/HAMM/HAMM-code.py
@@ -0,0 +1,21 @@
+#Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
+#Return: The Hamming distance dH(s,t)
+
+#opening file and separating by spaces into list
+DNA = open('githubhamm.txt')
+f = DNA.read()
+twostrings = f.split()
+
+#separating list into strings
+s = twostrings[0]
+t = twostrings[1]
+
+#counting mismatches
+count = 0 
+for i in range(len(stra)):
+	if s[i] == t[i]:
+		continue
+	else:
+		count = count + 1
+
+print(count)
