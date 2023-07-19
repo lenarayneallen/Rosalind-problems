7f153a4f8f3b7731df01423cd171a6bc305b2bc5
lenarayneallen <124638335+lenarayneallen@users.noreply.github.com>
2023-07-19 00:10:30 -0400

    Update REVC-code.py

--- Bioinformatics-stronghold/REVC/REVC-code.py
+++ Bioinformatics-stronghold/REVC/REVC-code.py
@@ -7,10 +7,10 @@ DNA_string = DNA.read()
 rev_DNA_string = DNA_string[::-1]
 
 
-def reverse_complement(DNA_string):
+def reverse_complement(sequence):
 	complement_dictionary = {"A":"T", "T":"A", "G":"C", "C":"G"}
 	complement_string = ""
-	for base in DNA_string:
+	for base in sequence:
 		if base in complement_dictionary:
 			complement_string += complement_dictionary[base]
 	print (complement_string)
