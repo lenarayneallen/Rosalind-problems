bfcc910de2f09e488e71d95e2884485d46783486
Lena Allen <124638335+lenarayneallen@users.noreply.github.com>
2024-02-11 15:16:41 -0500

    Update RNA-code.py

--- Bioinformatics-stronghold/RNA/RNA-code.py
+++ Bioinformatics-stronghold/RNA/RNA-code.py
@@ -1,4 +1,11 @@
+#Given: A DNA string t having length at most 1000 nt.
+#Return: The transcribed RNA string of t
+
+
+
+#open and read in file
 DNA = open ('RNA-input.txt')
 DNA_string = DNA.read()
+#replace all instances of thymine with uracil
 RNA = DNA_string.replace('T','U')
 print(RNA)
