1c82abee19cf8a05f084028e73cdb8b6808fb335
Lena Allen <124638335+lenarayneallen@users.noreply.github.com>
2024-02-11 15:04:44 -0500

    Update CONS_code.py

--- Bioinformatics-stronghold/CONS/CONS_code.py
+++ Bioinformatics-stronghold/CONS/CONS_code.py
@@ -1,3 +1,6 @@
+#Given: A collection of at most 10 DNA strings of equal length (at most 1 kbp) in FASTA format.
+#Return: A consensus string and profile matrix for the collection. (If several possible consensus strings exist, then you may return any one of them.)
+
 #parsing fasta file
 def parse_file(input_file):
 
