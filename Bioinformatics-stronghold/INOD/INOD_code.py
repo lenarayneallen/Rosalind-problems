924c605417a556773877c3982c15c1d45f3b64a2
Lena Allen <124638335+lenarayneallen@users.noreply.github.com>
2024-02-11 15:10:07 -0500

    Update INOD_code.py

--- Bioinformatics-stronghold/INOD/INOD_code.py
+++ Bioinformatics-stronghold/INOD/INOD_code.py
@@ -1,3 +1,7 @@
+#Given: A positive integer n (3≤n≤10000).
+
+#Return: The number of internal nodes of any unrooted binary tree having n leaves.
+
 s = open('INOD_input', 'r')
 p = s.read()
 n = int(p)
