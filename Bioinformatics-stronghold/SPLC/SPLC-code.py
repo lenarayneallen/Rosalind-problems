b63fbdfdb7dc1be5a97230b050ecaa11b92fa290
Lena Allen <124638335+lenarayneallen@users.noreply.github.com>
2024-02-11 15:17:28 -0500

    Update SPLC-code.py

--- Bioinformatics-stronghold/SPLC/SPLC-code.py
+++ Bioinformatics-stronghold/SPLC/SPLC-code.py
@@ -1,3 +1,9 @@
+#Given: A DNA string s(of length at most 1 kbp) and a collection of substrings of s acting as introns. All strings are given in FASTA format.
+
+#Return: A protein string resulting from transcribing and translating the exons of s.
+#(Note: Only one solution will exist for the dataset provided.)
+
+
 #open and parse codon table file 
 codontable = open('codontable.txt', 'r')
 codontable = codontable.read()
