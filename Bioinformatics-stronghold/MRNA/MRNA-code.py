a8fd0ac40aed486fb4cdf5734d7d403429e1318a
Lena Allen <124638335+lenarayneallen@users.noreply.github.com>
2024-02-11 15:11:36 -0500

    Update MRNA-code.py

--- Bioinformatics-stronghold/MRNA/MRNA-code.py
+++ Bioinformatics-stronghold/MRNA/MRNA-code.py
@@ -1,3 +1,8 @@
+#Given: A protein string of length at most 1000 aa.
+
+#Return: The total number of different RNA strings from which the protein could have been translated, modulo 1,000,000. 
+#(Don't neglect the importance of the stop codon in protein translation.)
+
 from functools import reduce
 
 
